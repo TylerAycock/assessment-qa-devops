7f89b5bc3b6ea148ed44d800f7a42dbe9a452f32
Tyler Aycock <tyler.joseph.aycock@gmail.com>
2023-06-25 10:30:55 -0700

    edited and completed assessment

--- __tests__/shuffle.test.js
+++ __tests__/shuffle.test.js
@@ -8,9 +8,15 @@ describe("shuffle should...", () => {
 test(`Shuffle should retun an arr`, ()=>{
     expect(shuffle()).toEqual([])
 })
-test(`Shuffle should return an arr with all the same items from the bots DB except shuffled`, ()=>{
-  console.info('the first bot from the shuffle ' + myBots[0].name)
-  console.log('the first bot from the database ' + botsData[0].name)
+
+test(`Shuffle should return an arr of the same length as the argument sent in`, ()=>{
   expect(myBots.length).toEqual(botsData.length)
 })
+
+test('Shuffle should return arr but in a random order', ()=>{
+  console.info('the first bot from the shuffle ' + myBots[0].name)
+  console.log('the first bot from the database ' + botsData[0].name)
+  expect(myBots[0].id).not.toEqual(botsData[0].id)
+})
+
 });
--- ec2.md
+++ ec2.md
@@ -1,2 +1,3 @@
 ## Paste your link below
 
+http://54.241.139.120:8000/
